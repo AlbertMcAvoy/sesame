52fb5ddcee14aee087fc85e95af1ebe05089ea39
Pellegrin69 <pellegrinpierre@hotmail.fr>
2024-07-16 16:07:00 +0200

    [ADD] - Database

--- backend/migrations/2024-06-28-075637_create_category/up.sql
+++
@@ -1,7 +0,0 @@
--- Your SQL goes here
-
-CREATE TABLE categories (
-    category_id uuid DEFAULT gen_random_uuid(),
-    name VARCHAR NOT NULL,
-    PRIMARY KEY (category_id) 
-);
---
+++ backend/migrations/2024-07-16-104006_create_water_closet/up.sql
@@ -0,0 +1,16 @@
+CREATE TYPE clean_state AS ENUM ('CLEANED', 'USED', 'DIRTY', 'OUT_OF_ORDER');
+
+CREATE TABLE
+    water_closets (
+        id SERIAL PRIMARY KEY,
+        group_id INTEGER NOT NULL REFERENCES groups (id),
+        is_disabled BOOLEAN NOT NULL DEFAULT false,
+        is_door_opened BOOLEAN NOT NULL DEFAULT false,
+        is_door_locked BOOLEAN NOT NULL DEFAULT false,
+        clean_state clean_state NOT NULL
+    );
+
+INSERT INTO
+    water_closets
+VALUES
+    (1, 1, true, true, false, 'CLEANED');
