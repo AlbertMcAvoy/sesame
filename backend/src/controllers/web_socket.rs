5650e25009637bf253baf3511a23decf9fac5222
myke <myke.chastang@orange.fr>
2024-07-17 17:20:31 +0200

    [EDIT] - Websocket
    Mise en place des messages pour Ouvrir et Fermer. Manque le métier

--- backend/src/controllers/web_socket.rs
+++ backend/src/controllers/web_socket.rs
@@ -4,12 +4,13 @@ use actix::*;
 use actix_web::{web, Error, HttpRequest, HttpResponse};
 use actix_web_actors::ws;
 
-use crate::ws::{server::Server, session::WsSession};
+use crate::{models::database::AppState, ws::{server::Server, session::WsSession}};
 
 pub async fn ws_route(
     req: HttpRequest,
     stream: web::Payload,
     srv: web::Data<Addr<Server>>,
+    state: web::Data<AppState>,
 ) -> Result<HttpResponse, Error> {
     ws::start(
         WsSession {
@@ -17,6 +18,7 @@ pub async fn ws_route(
             hb: Instant::now(),
             room: "main".to_owned(),
             addr: srv.get_ref().clone(),
+            state: state.get_ref().clone()
         },
         &req,
         stream,
