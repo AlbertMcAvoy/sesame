f07d1b615701d45e285e37a6ef4a7748d87e9915
Alex M <alexandremonierlycee@gmail.com>
2024-07-19 12:10:01 +0200

    [MAJ] affichage return ok

--- frontend/lib/pages/returnOk.dart
+++ frontend/lib/pages/returnOk.dart
@@ -15,12 +15,23 @@ class _ReturnOkState extends State<ReturnOk> {
   @override
   Widget build(BuildContext context) {
     returnHomeTimeout();
-    return Text("Votre retour a bien été envoyé ! ");
+    return const Padding(
+      padding: EdgeInsets.fromLTRB(0, 100, 0, 5),
+      child: DefaultTextStyle(
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 26,
+        ),
+        child: Text("Votre retour à bien été envoyé ! "),
+      ),
+    );
   }
-  Future<void> returnHomeTimeout() async {  
-    // Simulate a delay of 3 seconds  
-    await Future.delayed(Duration(seconds: 5));  
-      
+
+  Future<void> returnHomeTimeout() async {
+    // Simulate a delay of 3 seconds
+    await Future.delayed(Duration(seconds: 5));
+
     Navigator.pushNamed(context, '/');
-  }  
+  }
 }
