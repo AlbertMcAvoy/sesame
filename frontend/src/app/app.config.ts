c5a1c5f94e52d0932c4e4d33c60a3cbe658049ac
Myke Chastang <74027292+AlbertMcAvoy@users.noreply.github.com>
2024-07-30 09:55:22 +0200

    [ADD] - PrimeNG (#44)
    Intégration de PrimeNG et PrimeFlex pour mettre à disposition un UI Kit et un bootstrap like

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -11,6 +11,7 @@ import {
   GoogleLoginProvider,
 } from '@abacritt/angularx-social-login';
 import { environment } from '../environments/environment';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -21,6 +22,7 @@ export const appConfig: ApplicationConfig = {
       withFetch(),
       withInterceptors([authInterceptor])
     ),
+    provideAnimations(),
     {
       provide: 'SocialAuthServiceConfig',
       useValue: {
